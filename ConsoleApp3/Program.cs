9d3bdb3507070d065709f380ceadf6ccacc886cc
Boubakr Echieb <echieb.boubakr@gmail.com>
2018-04-20 23:50:03 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        public class Rate
+        {
+            public string Category { get; set; }
+            public DateTime DateTime { get; set; }
+            public decimal Value { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            foreach (var item in ReadRates("C:/Users/BE/Source/Repos/scharpCertification/ConsoleApp3/rateXml.xml"))
+            {
+                Console.WriteLine("rate Category : {0}, rate Date : {1}, rate value : {2}", item.Category, item.DateTime, item.Value);
+                Console.WriteLine("*****************************************");
+            }
+            Console.ReadKey();
+        }
+        private static Collection<Rate>  ReadRates(string ratesXml)
+        {
+            Collection<Rate> rateCollection = new Collection<Rate>();
+            try
+            {
+                using (XmlReader reader = XmlReader.Create(new StreamReader(ratesXml)))
+                {
+                    while (reader.ReadToFollowing("rate"))
+                    {
+                        Rate rate = new Rate();
+                        reader.MoveToFirstAttribute();
+                        rate.Category = reader.Value;
+                        reader.MoveToNextAttribute();
+                        DateTime rateDate;
+                        if (DateTime.TryParse(reader.Value, out rateDate))
+                        {
+                            rate.DateTime = rateDate;
+                        }
+                        reader.ReadToFollowing("value");
+                        decimal rateValue;
+                        if (Decimal.TryParse(reader.ReadElementContentAsString(), out rateValue))
+                        {
+                            rate.Value = rateValue;
+                        }
+                        rateCollection.Add(rate);
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                throw exp;
+            }
+            return rateCollection;
+        }
+    }
+}
---
+++ scharpCertification/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace scharpCertification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateInterrest(12, 8, 5);
+            Console.ReadKey();
+        }
+        public static decimal CalculateInterrest(decimal loanAmount, int loanTerm, decimal loanRate)
+        {
+            decimal interrestAmount = loanAmount * loanRate * loanRate;
+//#if DEBUG
+            Log("message", "details");
+//#endif
+            return interrestAmount;
+        }
+        [Conditional("DEBUG")]
+        public static void Log(string message, string details)
+        {
+            Console.WriteLine("message : {0}, details : {1}", message, details);
+        }
+    }
+}
